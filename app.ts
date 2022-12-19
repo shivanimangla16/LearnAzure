e9ac44f5fc7e3858afef3c01ce05edd239e99810
shivanimangla16 <shivani_mangla@hcl.com>
2022-12-19 11:20:56 +0530

    ngx chart to pdf conversion done

--- app.ts
+++ app.ts
@@ -1,69 +1,99 @@
 import { TestBed } from '@angular/core/testing';
-import express, { Request, Response,NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+const router = express.Router();
+const nodeHtmlToImage = require('node-html-to-image');
+
+const htmlToBase64Image = require('html-to-base64-image');
 
 const app = express();
 var path = require('path');
 const port = 4000;
 app.use(express.json({ limit: '50mb' }));
-var allowCrossDomain = function(req:Request, res:Response, next:NextFunction) {
-  res.header('Access-Control-Allow-Origin', "*");
+var allowCrossDomain = function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
-}
+};
 app.use(allowCrossDomain);
 app.listen(port);
+// console.log(req.body.data);
+// res.json({"data":"test"});
+// const image = await nodeHtmlToImage({
+//   // html: '<html><body><div>Check out what I just did! #cool</div></body></html>'
+//   html: req.body.data
+// });
+// res.writeHead(200, { 'Content-Type': 'image/png' });
+// res.end(image);
 
+// var data = req.body.data;
 
 const getPdf = async (req: Request, res: Response) => {
-  var data = req.body.data;
-  var fonts = {
-    Roboto: {
-      normal: path.resolve('src', 'fonts', 'Roboto.ttf'),
-      bold: path.resolve('src', 'fonts', 'Roboto-Bold.ttf'),
-    },
-  };
+  const image = await nodeHtmlToImage({
+    // html: '<html><body><div class="row"><div>Check out what I just did! #cool</div><div>Check out what I just did! #cool</div></div></body></html>'
+    // html: '<html><body><div><input/><input/></div></body></html>',
+    html:req.body.data
+  });
+  // console.log(image);
+  const base64Image = Buffer.from(image).toString('base64');
+  const dataURI = 'data:image/jpeg;base64,' + base64Image;
+  console.log(dataURI);
+  res.json({ data: 'wygdu' });
+  return;
 
-  var PdfPrinter = require('pdfmake');
-  var printer = new PdfPrinter(fonts);
-
-  const docDefinition = {
-    info: {
-      title: 'PDF with External Image',
-      author: 'Matt Hagemann',
-      subject: 'PDF with External Image',
-    },
-    content: [
-      {
-        image: data,
-        width: 595, // Full A4 size width.
-        absolutePosition: { x: 0, y: 0 },
+  htmlToBase64Image(req.body.data).then(async (data: any) => {
+    // console.log(data);
+    var fonts = {
+      Roboto: {
+        normal: path.resolve('src', 'fonts', 'Roboto.ttf'),
+        bold: path.resolve('src', 'fonts', 'Roboto-Bold.ttf'),
       },
-    ],
-    defaultStyle: {
-      fontSize: 11,
-      font: 'Roboto', // The font name was defined above.
-      lineHeight: 1.2,
-    },
-  };
-  try {
-    var pdfDoc = await printer.createPdfKitDocument(docDefinition, {});
-  } catch (error) {
-    console.log('That did not go well.')
-    console.log(error);
-  }
-  var chunks: Uint8Array[] = [];
-  await pdfDoc.on('data', (val: Uint8Array) => {
-    chunks.push(val);
+    };
+    var PdfPrinter = require('pdfmake');
+    var printer = new PdfPrinter(fonts);
+    //  var temp = PdfPrinter.pageLayout;
+    //  console.log(temp);
+    const docDefinition = {
+      pageSize: 'A4',
+      pageMargins: [50, 50, 50, 50],
+      info: {
+        title: 'PDF with External Image',
+        author: 'Matt Hagemann',
+        subject: 'PDF with External Image',
+      },
+      content: [
+        {
+          image: data,
+          width: 500,
+        },
+        {
+          image: data,
+          width: 500,
+        },
+      ],
+    };
+    try {
+      var pdfDoc = await printer.createPdfKitDocument(docDefinition, {});
+    } catch (error) {
+      console.log('That did not go well.');
+      console.log(error);
+    }
+    var chunks: Uint8Array[] = [];
+    await pdfDoc.on('data', (val: Uint8Array) => {
+      chunks.push(val);
+    });
+    await pdfDoc.on('end', function () {
+      var result = Buffer.concat(chunks);
+      var str = result.toString('base64');
+      var json = { resposne: str };
+      console.log(json);
+      res.json(json);
+    });
+    pdfDoc.end();
   });
-  await pdfDoc.on('end', function () {
-    var result = Buffer.concat(chunks);
-    var str = result.toString('base64');
-    var json = {"resposne":str};
-    res.json(json);
-  });
-  pdfDoc.end();
-
 };
 app.post('/', getPdf);
-
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,25 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { single, multi } from './data';
-import html2canvas from 'html2canvas';
-import domtoimage from 'dom-to-image';
 import * as htmlToImage from 'html-to-image';
-import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { NgxService } from './services/ngx.service';
-import { getTestBed } from '@angular/core/testing';
-
+import { LegendPosition } from '@swimlane/ngx-charts';
 var pdfMake = require('pdfmake/build/pdfmake');
-var pdfFonts = require('pdfmake/build/vfs_fonts');
+// var pdfFonts = require('pdfmake/build/vfs_fonts');
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
   single?: any[];
   multi?: any[];
   docDefinition?: any;
   view: any[] = [700, 400];
-  // options
+  title: string = 'Legend';
+  below = LegendPosition.Right;
+  base64StringArray?:string[] = [];
   showXAxis = true;
   showYAxis = true;
   gradient = false;
@@ -30,7 +28,6 @@ export class AppComponent {
   xAxisLabel = 'Country';
   showYAxisLabel = true;
   yAxisLabel = 'Population';
-
   colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
   };
@@ -40,28 +37,28 @@ export class AppComponent {
   }
 
   ngOnInit() {}
-  getTest() {
-    var node = document.getElementById('chart');
-    // var img = new Image();
-    // img.src = dataUrl;
-    // img.style.height = '1000px';
-    // img.style.height = '845px';
-    // console.log(console.log(node!.getBoundingClientRect()));
-    document.body.appendChild(node!);
-    console.log(node!.outerHTML);
-
-    // this.ngxService.getPdf("").subscribe((res) => {
-    //   console.log(res);
-    // });
-  }
-
   async downloadChart() {
     console.log('download button clicked');
-    var node = document.getElementById('chart');
-    var base64ImageStr = '';
-    // console.log(node!.outerHTML);
+    var div1 = document.getElementById('chart');
+    var div2 = document.getElementById('chart2');
 
-    await htmlToImage
+    var arr : HTMLElement[] =[div1!,div2!];
+    console.log(arr.length);
+    for (const element of arr) {
+      await this.convertHtmlToImage(element);
+    }
+    console.log("shivani");
+    console.log(this.base64StringArray!.length);
+    if(this.base64StringArray!.length > 0)
+    {
+      this.getPdf();
+    }
+  }
+
+  async convertHtmlToImage(node:HTMLElement)
+  {
+    var base64ImageStr = '';
+     await htmlToImage
       .toPng(node!)
       .then(function (dataUrl) {
         base64ImageStr = dataUrl;
@@ -69,15 +66,39 @@ export class AppComponent {
       .catch(function (error) {
         console.error('oops, something went wrong!', error);
       });
-    var dataUrl = '';
-    await this.ngxService.getPdf(base64ImageStr).subscribe(async (res) => {
-      dataUrl = res['resposne'];
-      console.log(res);
-      const link = await document.createElement('a');
-      const source = `data:application/pdf;base64,${dataUrl}`;
-      link.href = source;
-      link.download = 'test.pdf';
-      link.click();
+      console.log(base64ImageStr);
+      if (base64ImageStr != '') {
+         this.base64StringArray!.push(base64ImageStr);
+      }
+      // console.log("mangla");
+    console.log(this.base64StringArray!.length);
+
+  }
+
+  async getPdf() {
+    var iterator = this.base64StringArray!.values();
+
+    const docDefinition = {
+      content: [
+        {
+          image: iterator.next().value,
+          width: 520,
+        },
+      ],
+    };
+
+    this.base64StringArray!.forEach((item, index, array) => {
+      if(index >0)
+      {
+        docDefinition.content!.push({ image: item!, width: 520});
+      }
     });
+    console.log(this.base64StringArray!.length);
+    if (docDefinition) {
+      pdfMake.createPdf(docDefinition).download('chartToPdf' + '.pdf');
+      pdfMake.createPdf(docDefinition).open();
+    } else {
+      console.log('Chart is not yet rendered!');
+    }
   }
 }
